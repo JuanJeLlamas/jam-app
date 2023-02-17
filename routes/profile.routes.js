a536708d26013eeea230d0d12c5ec31be0146420
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-02-17 18:51:51 +0200

    Validaciones,Done!

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,47 +2,144 @@ const express = require('express');
 const router = express.Router();
 
 const User = require("../models/User.model.js");
+const bcrypt = require("bcryptjs");
 
 
 
 
+//GET /profile/signup----------------------
 
-//GET /profile/signin
-router.get("/signin", (req, res, next) => {
 
-    res.render("user/signin.hbs")
+router.get("/signup", (req, res, next) => {
+
+    res.render("user/signup.hbs")
 })
 
-//POST /profile/signin
-router.post("/signin", async (req, res, next) => {
+//POST /profile/signup----------------------
+
+
+router.post("/signup", async (req, res, next) => {
     const { username, email, password } = req.body
 
     if (username === "" || email === "" || password === "") {
-        res.status(401).render("user/signin.hbs", {
+        res.status(401).render("user/signup.hbs", {
             errorMessage: "Todos los campos son obligatorios",
         });
         return
     }
 
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+    if (passwordRegex.test(password) === false) {
+        res.render("user/signup.hbs", {
+            errorMessage:
+                "La contraseña debe tener un mininmo 8 caracteres, una mayuscula, una minuscula y un caracter especial",
+        });
+        return;
+    }
+
+
     try {
+        const correctUser = await User.findOne({ username: username });
+
+        if (correctUser !== null) {
+            res.render("user/signup.hbs", {
+                errorMessage: "Nombre de usuario ya existe",
+            });
+            return;
+        }
+
+        const correctEmail = await User.findOne({ email: email });
+       
+        console.log(correctEmail);
+        if (correctEmail !== null) {
+            res.render("user/signup.hbs", {
+                errorMessage: "Ya hay una cuenta creada con esta direccion de correo",
+            });
+            return; 
+        }
+
+        const salt = await bcrypt.genSalt(12);
+        const hashPassword = await bcrypt.hash(password, salt);
+    
+
+
+
         const trueUser = await User.create({
-            username : username,
+            username: username,
             email: email,
-            password : password
+            password: hashPassword
         })
 
-
-
-
-            res.redirect("/ ")
+        res.redirect("/profile/login")
     } catch (error) {
         next(error)
     }
 
+})
+
+// GET /profile/login----------------------
+
+
+router.get("/login", (req, res, next) => {
+
+    res.render("user/login.hbs")
+})
+
+
+// POST /profile/login---------------------
+
+
+
+router.post("/login", async (req, res, next) => {
+    const { username, password } = req.body
+
+    if (username === "" || password === "") {
+        res.render("user/login.hbs", {
+            errorMessage: "Todos los campos son obligatorios"
+        })
+        return
+    }
+    try {
+        const userLogin = await User.findOne({ username })
+        if (userLogin === null) {
+            res.render("user/login.hbs", {
+                errorMessage: "Usuario no encontrado"
+            })
+            return
+        }
+
+        const correctPassword = await bcrypt.compare(password, userLogin.password)
+        if (correctPassword === false) {
+            res.render("user/login.hbs", {
+                errorMessage: "Contraseña incorrecta",
+            });
+            return;
+        }
+
+        res.redirect("/")
+    }
+
+    catch (error) {
+        next(error)
+    }
 
 
 })
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
