c5b7b86314b6aa33a7880d248fea81c5629efb09
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-20 12:42:14 +0100

    Error en renderizar perfil

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const User = require('../models/User.model');
 const router = express.Router();
 
-// GET "/groups/"
+// GET "/groups/list"
+router.get("/list", async (req, res, next) => {
+    try {
+      const response = await User.find();
+  
+      res.render("groups/list-artist.hbs", {
+        eachArtist: response,
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
 
 module.exports = router;
