c6f3ee428b341344d72462dd38e683af7983f376
JuanJe <juanje.llamas@gmail.com>
2023-02-21 15:51:16 +0100

    git ignore done

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -3,9 +3,10 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
  
 cloudinary.config({
-  cloud_name: "dsevu38bc",
-  api_key: "394947273272951",
-  api_secret: "pD90JwjVKr5zcHrquJbGTr2fp8I"
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+   api_secret: process.env.CLOUDINARY_SECRET
+  
 });
  
 const storage = new CloudinaryStorage({
