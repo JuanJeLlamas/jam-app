ee0c26146306633bbf0befef4759ef194887102e
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-02-21 08:43:42 +0200

    Bug de formulario corregido

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,41 +1,33 @@
-const express = require('express');
-const User = require('../models/User.model');
-const router = express.Router();
+const express = require('express')
+const User = require('../models/User.model')
+const router = express.Router()
 
 // GET "/groups/list"
-router.get("/list", async (req, res, next) => {
+router.get('/list', async (req, res, next) => {
   // const { role } = req.params;
- 
-    try {
 
-      const response = await User.find( {role: {$in: ["artista"] } } );
-  
-      res.render("groups/list-artist.hbs", {
-        eachArtist: response,
-      });
-      
-    }
-    
-    catch (err) {
-      next(err);
-    }
-    
-  });
+  try {
+    const response = await User.find({ role: { $in: ['artista'] } })
 
+    res.render('groups/list-artist.hbs', {
+      eachArtist: response,
+    })
+  } catch (err) {
+    next(err)
+  }
+})
 
+router.get('/:id/details', async (req, res, next) => {
+  try {
+    const { id } = req.params
 
-  router.get("/:id/details", async (req, res, next) => {
-    try {
-      const { id } = req.params;
-  
-      const artistDetails = await User.findById(id);
-      res.render("groups/details-artist.hbs", {
-        artistDetails: artistDetails,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+    const artistDetails = await User.findById(id)
+    res.render('groups/details-artist.hbs', {
+      artistDetails: artistDetails,
+    })
+  } catch (error) {
+    next(error)
+  }
+})
 
-
-module.exports = router;
+module.exports = router
