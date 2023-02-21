ad8e9069a0fc0cbe8a6123c7ccc4b70e3ff7d5ec
JuanJe <juanje.llamas@gmail.com>
2023-02-21 12:26:42 +0100

    comments Done

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+
+    try{
+        res.render("user/comments.hbs");
+    }
+    catch (error){
+        next(error)
+    }
+       
+      });
+
+
+
+
+
 
 
 module.exports = router;
+
