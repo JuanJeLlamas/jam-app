ce017dafa0301ccfc8855cd4f8fa4f1e8d11fc05
JuanJe <juanje.llamas@gmail.com>
2023-02-21 20:01:07 +0100

    relacion reseñas en proceso

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const Comments = require('../models/comments.model')
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
 
+router.get('/', (req, res, next) => {
+    res.render("user/comments.hbs");
+  })
+
+router.post('/', async (req, res, next) => {
+
+    const comment = req.body.comment
+    console.log("hola" , comment)
+  
     try{
-        res.render("user/comments.hbs");
+        const response = await Comments.find()
+        res.render("user/comments.hbs", {
+        comment: response
     }
+    )} 
     catch (error){
         next(error)
     }
