665cfd76cf97c1e0a6ea469e4fa71b55d836d750
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-22 18:10:00 +0100

    Crear funcionalidades comentarios

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const User = require('../models/User.model')
+const Comments = require("../models/comments.model");
 const router = express.Router()
 
-// GET "/groups/list"
+// GET "/groups/list" => renderiza el listado de grupos que existen según el género musical escogido"
 router.get('/list/:genre', async (req, res, next) => {
   
   const genre = req.params.genre
@@ -19,13 +20,18 @@ router.get('/list/:genre', async (req, res, next) => {
   }
 })
 
+// GET "/groups/:id/details" => renderiza los detalles del grupo musical seleccionado
 router.get('/:id/details', async (req, res, next) => {
   try {
-    const { id } = req.params
-
+    const { id } = req.params;
+    
     const artistDetails = await User.findById(id)
+    const commentDetails = await Comments.find({ artistUser: id }).populate("creator")
+    
     res.render('groups/details-artist.hbs', {
       artistDetails: artistDetails,
+      commentDetails: commentDetails,
+  
     })
   } catch (error) {
     next(error)
