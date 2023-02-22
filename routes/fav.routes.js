3924f04fcf9445b07b7bedb14f6958f0c56862aa
JuanJe <juanje.llamas@gmail.com>
2023-02-22 23:07:31 +0100

    favorites in project

--- routes/fav.routes.js
+++ routes/fav.routes.js
@@ -2,34 +2,40 @@ const express = require('express')
 const User = require('../models/User.model')
 const router = express.Router()
 
-router.post("/:id", async (req, res, next) => {
-const artistId = req.params
-console.log("HOLaaAQ" , artistId)   //Necesitamos requerir la id del artista para añadirlo al array del usuario. Se hara redirect al mismo lugar despues de 
-    try {
-        
-        let response = await User.findById(artistId)
-        res.redirect("/groups/list/")
+router.get("/", async (req, res, next) => {
+ const userParticular = req.session.activeUser
+ try{
 
-    } catch (err) {
-        next(err)
-    }
+    const response = await User.findById(userParticular._id)
+    .populate( {path: "favourite" } );
+     res.render("user/fav-list.hbs", { 
+        response : response
+        
+     })
+    
+ } catch(err){
+    next(err)
+ }
+   //Necesitamos requerir la id del artista para añadirlo al array del usuario. Se hara redirect al mismo lugar despues de 
 
 
 })
 
-// router.post(`/groups/:id/favorites`, async (req, res, next) => {
-// //creo que deberia ser ruta dinamica por id del usuario activo
-//    const actualUser = req.session.activeUser 
-//    const userId = req.params
-//    try{
-//     await User.findByIdAndUpdate(activeUser._id, {
-//         $addToSet: {favourite: userId}
-//     })
-//    } catch (err){
-//     next(err)
-//    }
 
-// })
+router.post("/:id", async (req, res, next) => {
+//creo que deberia ser ruta dinamica por id del usuario activo
+   const { id } = req.params
+   const userParticular = req.session.activeUser
+   try{
+    await User.findByIdAndUpdate(userParticular._id, {
+        $addToSet: { favourite: id }
+    });
+    res.redirect("/")
+   } catch (err){
+    next(err)
+   }
+
+})
 
 module.exports = router;
 
